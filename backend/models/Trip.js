41b982325b40354a7ca28e7d3920fc12ab7fb78c
Raymond <raymondtan676@gmail.com>
2023-10-23 12:03:45 -0700

    Add basic backend for trips

---
+++ backend/models/Trip.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tripSchema = new Schema({
+    driver: {
+        type: Schema.Types,ObjectId,
+        ref: 'User'
+    },
+    passengers: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'User', 
+    }],
+    startPoint: {
+        type: String,
+        required: true,
+    },
+    endPoint: {
+        type: String,
+        required: true,
+    },
+    passengerLimit: {
+        type: Number,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model('Trip', tripSchema);
