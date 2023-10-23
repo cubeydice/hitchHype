432f7bb88542cd8fd98b86cbd2b94e592fa6c006
Brittiny Filbert <britfilbert@icloud.com>
2023-10-23 14:14:12 -0700

    add trips to store

---
+++ frontend/src/components/TripPage/TripItem.js
@@ -0,0 +1,21 @@
+
+
+export function TripItem ({ trip }) {
+
+
+    return (
+        <div className="TripItem-container">
+            <div className="TripItem-photo">
+                {/* {trip.} */}
+            </div>
+            <div className="TripItem-details">
+                <div className="TripItem-destination">
+
+                </div>
+                <div className="TripItem-price">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,12 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session';
-import tweets from './tweets';
+import trips from './trips';
 
 
 const rootReducer = combineReducers({
     session,
-    tweets
+    trips
 });
 
 let enhancer;
