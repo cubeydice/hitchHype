ff2dc2bca7a06c69ed289cdcf9b96fb4bc8e579a
Raymond <raymondtan676@gmail.com>
2023-10-24 11:04:58 -0700

    Add owner to car schema model

--- backend/models/Car.js
+++ backend/models/Car.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const carSchema = new Schema({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
     make: {
         type: String,
         required: true,
