445e7f818c937c1340eab29b17ef963e8a02c5eb
Raymond <raymondtan676@gmail.com>
2023-10-25 14:20:02 -0700

    Change address schema to simply string, remove and refactor broken validation, remove broken format date

--- backend/routes/api/trips.js
+++ backend/routes/api/trips.js
@@ -1,51 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
-const Trip = mongoose.model('Trip');
-const { requireUser } = require('../../config/passport');
-const validateTripInput = require('../../validations/trip');
-
-// const formatDate = (date) => {
-//     const month = (date.getMonth() + 1).toString().padStart(2, '0');
-//     const day = date.getDate().toString().padStart(2, '0');
-//     const year = date.getFullYear();
-//     const hours = date.getHours().toString().padStart(2, '0');
-//     const minutes = date.getMinutes().toString().padStart(2, '0');
-//     const seconds = date.getSeconds().toString().padStart(2, '0');
-
-//     return `${month}-${day}-${year} ${hours}:${minutes}:${seconds}`;
-// }
+const mongoose = require("mongoose");
+// const parserAddress = require('parse-address-string');
+const User = mongoose.model("User");
+const Trip = mongoose.model("Trip");
+const { requireUser } = require("../../config/passport");
+const validateTripInput = require("../../validations/trip");
 
 // path = /trips
 // Retrieve all trips
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
     try {
         const trips = await Trip.find()
                                 .populate("driver", "_id firstName lastName")
-                                .populate('car', 'make model year' )
+                                .populate("car", "make model year maxPassengers licensePlateNumber insurance mpg fueleconomyId" )
                                 .populate("passengers.passenger", "_id firstName lastName")
                                 .sort({ createdAt: -1 });
-        const formattedTrips = trips.map((trip) => {
-            return {
-                ...trip.toObject(),
-                departureDate: formatDate(trip.departureDate)
-            }
-        })
-        return res.json(formattedTrips);
+        return res.json(trips);
     }
     catch(err) {
         return res.json([]);
     } 
 });
 
-// Retrieve user's trips
-router.get('/user/:userId', async (req, res, next) => {
+// Retrieve user"s trips
+router.get("/user/:userId", async (req, res, next) => {
     let user;
     try {
         user = await User.findById(req.params.userId);
     } catch(err) {
-        const error = new Error('User not found');
+        const error = new Error("User not found");
         error.statusCode = 404;
         error.errors = { message: "No user found with that id" };
         return next(error);
@@ -54,15 +38,9 @@ router.get('/user/:userId', async (req, res, next) => {
         const trips = await Trip.find({ driver: user._id })
                                 .sort({ createdAt: -1 })
                                 .populate("driver", "_id firstName lastName")
-                                .populate('car', 'make model year' )
+                                .populate("car", "make model year maxPassengers licensePlateNumber insurance mpg fueleconomyId" )
                                 .populate("passengers.passenger", "_id firstName lastName");
-        const formattedTrips = trips.map((trip) => {
-            return {
-                ...trip.toObject(),
-                departureDate: formatDate(trip.departureDate)
-            }
-        }) 
-        return res.json(formattedTrips);
+        return res.json(trips);
     }
     catch(err) {
         return res.json([]);
@@ -70,20 +48,16 @@ router.get('/user/:userId', async (req, res, next) => {
 })
 
 // Retrieve individual trip
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
     try {
         const trip = await Trip.findById(req.params.id)
                                 .populate("driver", "_id firstName lastName")
-                                .populate('car', 'make model year' )
+                                .populate("car", "make model year maxPassengers licensePlateNumber insurance mpg fueleconomyId" )
                                 .populate("passengers.passenger", "_id firstName lastName");
-        const formattedTrip = {
-            ...trip.toObject(),
-            departureDate: formatDate(trip.departureDate)
-            }
-        return res.json(formattedTrip);
+        return res.json(trip);
     }
     catch(err) {
-        const error = new Error('Trip not found');
+        const error = new Error("Trip not found");
         error.statusCode = 404;
         error.errors = { message: "No trip found with that id" };
         return next(error);
@@ -92,13 +66,11 @@ router.get('/:id', async (req, res, next) => {
 
 // Create a trip
 // Validation middleware should accept vehicle passenger limit as max
-router.post('/', requireUser, validateTripInput, async (req, res, next) => {
+router.post("/", requireUser, validateTripInput, async (req, res, next) => {
     try {
         // Extract the required data from the request
         const { user, body } = req;
         const { car, departureDate, origin, destination, availableSeats } = body;
-
-        // const formarttedDepartureDate = formatDate(departureDate);
 
         const newTrip = new Trip({
             driver: user._id,
@@ -112,8 +84,8 @@ router.post('/', requireUser, validateTripInput, async (req, res, next) => {
     
         const trip = await newTrip.save();
         const populatedTrip = await Trip.populate(trip, [
-            { path: 'driver', select: '_id firstName lastName' },
-            { path: 'car', select: 'make model year' },
+            { path: "driver", select: "_id firstName lastName" },
+            { path: "car", select: "make model year maxPassengers licensePlateNumber insurance mpg fueleconomyId" },
         ]);
         return res.json(populatedTrip);
     }
@@ -130,7 +102,7 @@ router.patch('/:id', requireUser, validateTripInput, async (req, res, next) => {
         const trip = await Trip.findById(req.params.id);
 
         if (!trip) {
-            const error = new Error('Trip not found');
+            const error = new Error("Trip not found");
             error.status = 404;
             error.errors = { message: "No trip found with that id" };
             return next(error);
@@ -140,31 +112,31 @@ router.patch('/:id', requireUser, validateTripInput, async (req, res, next) => {
 
         // Check if the user is the driver of the trip
         if (trip.driver.toString() !== user._id.toString()) {
-            const error = new Error('Unauthorized: You are not the driver of the trip');
+            const error = new Error("Unauthorized: You are not the driver of the trip");
             error.status = 403;
-            error.errors = { message: 'You are not the driver of the trip' }
+            error.errors = { message: "You are not the driver of the trip" }
             return next(error);
         }
         
         // Extract the required data from the request
         const { car, passengers, departureDate, origin, destination, availableSeats } = body;
-    
-        const formarttedDepartureDate = formatDate(departureDate);
 
         // Update the trip with the new data
         trip.passengers = passengers;
         trip.car = car;
-        trip.departureDate = formarttedDepartureDate;
+        trip.departureDate = departureDate;
         trip.origin = origin;
         trip.destination = destination;
         trip.availableSeats = availableSeats;
 
         // Save the updated trip
-        let updatedTrip = await trip.save();
-        updatedTrip = await trip.populate('driver', '_id firstName lastName')
-                                .populate('car', 'make model year licensePlateNumber' )
-                                .populate("passengers.passenger", "_id firstName lastName");
-        res.json(updatedTrip);
+        const updatedTrip = await trip.save();
+        const populatedTrip = await Trip.populate(updatedTrip, [
+            { path: "driver", select: "_id firstName lastName" },
+            { path: "car", select: "make model year maxPassengers licensePlateNumber insurance mpg fueleconomyId" },
+            { path: "passengers.passenger", select: "_id firstName lastName" },
+        ]);
+        res.json(populatedTrip);
     }
     catch(err) {
         next(err);
@@ -172,13 +144,13 @@ router.patch('/:id', requireUser, validateTripInput, async (req, res, next) => {
 });
 
 // Remove trip
-router.delete('/:id', requireUser, validateTripInput, async (req, res, next) => {
+router.delete("/:id", requireUser, validateTripInput, async (req, res, next) => {
     try {
         // Find the trip by its ID
         const trip = await Trip.findById(req.params.id);
         
         if (!trip) {
-            const error = new Error('Trip not found');
+            const error = new Error("Trip not found");
             error.status = 404;
             error.errors = { message: "No trip found with that id" };
             return next(error);
@@ -186,15 +158,15 @@ router.delete('/:id', requireUser, validateTripInput, async (req, res, next) => 
 
         // Check if the user is the driver of the trip
         if (trip.driver.toString() !== req.user._id.toString()) {
-            const error = new Error('Unauthorized: You are not the driver of the trip');
+            const error = new Error("Unauthorized: You are not the driver of the trip");
             error.status = 403;
-            error.errors = { message: 'You are not the driver of the trip' }
+            error.errors = { message: "You are not the driver of the trip" }
             return next(error);
         }
 
         // Remove the trip from the database
-        await trip.remove();
-        res.json({ message: 'Trip deleted successfully' });
+        await trip.deleteOne();
+        res.json({ message: "Trip deleted successfully" });
     }
     catch(err) {
         next(err);
--- backend/validations/trip.js
+++ backend/validations/trip.js
@@ -1,21 +1,6 @@
 const { check } = require("express-validator");
 const handleValidationErrors = require('./handleValidationErrors');
 
-// Custom validation function to check date format
-// const isValidDate = (value) => {
-//     const dateRegex = /^\d{2}-\d{2}-\d{4}$/; // Assuming MM-DD-YYYY
-//     return dateRegex.test(value);
-// }
-
-// Custom validation function to limit max available seats to car passenger limit
-const validateAvailableSeats = (value, { req }) => {
-    const maxPassengers = req.body.car.maxPassengers; // Assuming you've passed the car object in the request
-    if (value > maxPassengers) {
-        throw new Error(`Available seats cannot exceed ${maxPassengers}`);
-    }
-    return true;
-}
-
 const validateTripInput = [
     check('driver')
         .exists({ checkFalsy: true })
@@ -37,49 +22,18 @@ const validateTripInput = [
         }
         next();
     },
-    // check('passengers.passenger')
-    //     .exists({ checkFalsy: true })
-    //     .withMessage('Passenger is required'),
-    // check('passengers.dropoffPoint')
-    //     .exists({ checkFalsy: true })
-    //     .withMessage('Drop-off point is required'),
     check('departureDate')
         .exists({ checkFalsy: true })
         .withMessage('Date is required'),
-        // .custom((value) => {
-        //     if (!isValidDate(value)) {
-        //         throw new Error('Invalid date format');
-        //     }
-        //     return true;
-        // }),
-    check('origin.street')
+    check('origin')
         .exists({ checkFalsy: true })
-        .withMessage('Start point street is required'),
-    check('origin.city')
+        .withMessage('Origin is required'),
+    check('destination')
         .exists({ checkFalsy: true })
-        .withMessage('Start point city is required'),
-    check('origin.state')
-        .exists({ checkFalsy: true })
-        .withMessage('Start point state is required'),
-    check('origin.postalCode')
-        .exists({ checkFalsy: true })
-        .withMessage('Start point postal code is required'),
-    check('destination.street')
-        .exists({ checkFalsy: true })
-        .withMessage('End point street is required'),
-    check('destination.city')
-        .exists({ checkFalsy: true })
-        .withMessage('End point city is required'),
-    check('destination.state')
-        .exists({ checkFalsy: true })
-        .withMessage('End point state is required'),
-    check('destination.postalCode')
-        .exists({ checkFalsy: true })
-        .withMessage('End point postal code is required'),
+        .withMessage('Destination is required'),
     check('availableSeats')
         .isInt({ min: 0 }) // max limit dependent on driver's vehicle
-        .withMessage('Available seats must be a positive integer')
-        .custom(validateAvailableSeats),
+        .withMessage('Available seats must be a positive integer'),
     handleValidationErrors
 ];
 
