fb862d96c61ba083a9823be2ace82aa24f49bb72
Brittiny Filbert <britfilbert@icloud.com>
2023-10-26 01:11:55 -0700

    add basic components

--- frontend/src/components/Trips/Driver/DriverTripUpdate.js
+++ frontend/src/components/Trips/Driver/DriverTripUpdate.js
@@ -3,46 +3,56 @@ import { Passenger } from "../Passenger/Passenger";
 import { useDispatch, useSelector } from "react-redux";
 import { clearTripErrors, fetchTrip } from "../../../store/trips";
 import { useEffect } from "react";
-
+import "./DriverTripUpdate.css"
 
 
 export function DriverUpdateForm () {
     const { tripId } = useParams();
-    const dispatch = useDispatch;
+    console.log(tripId)
+    const dispatch = useDispatch();
     const trip = useSelector(state => state.trips);
-    const sesionUser = useSelector(state => state.session.user);
+    // const sesionUser = useSelector(state => state.session.user);
 
     useEffect( () => {
-        dispatch(fetchTrip(tripId))   //.then( trip => console.log(trip))
+        dispatch(fetchTrip(tripId));   //.then( trip => console.log(trip))
         dispatch(clearTripErrors());
     }, [dispatch])
 
 
     return (
-        <div className="Driver-update-page-layout">
-            <div className="Driver-update-header">
-                <h3>Update your trip!</h3>
-            </div>
-            <div className="Driver-update-map-container">
-
-            </div>
-            <div className="Driver-update-passengers-container">
-                <div className="Driver-update-passengers-header">
-                    <h3>Passengers</h3>
+        <>
+            { trip.passengers ? (
+                <div className="Driver-update-page-layout">
+                    <div className="Driver-update-header">
+                        <h3 className="Driver-update-header-h3">Update your trip!</h3>
+                    </div>
+                    <div className="Driver-update-map-container">
+                        <div className="map-api">Map Api</div>
+                    </div>
+                    <div className="Driver-update-passengers-container">
+                        <div className="Driver-update-passengers-header">
+                            <h3 className="Driver-update-header-h3">Passengers</h3>
+                        </div>
+                        <div className="Driver-update-passengers-items">
+                            {trip.passengers.map( passenger => (
+                                <>
+                                    <Passenger key={passenger.passenger._id} passenger={passenger} className="Driver-update-passenger-item"/>
+                                    <div className="Driver-update-rmv-passenger-container">
+                                        <button id="Driver-update-rmv-passenger">Remove passenger</button>
+                                    </div>
+                                </>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="Driver-update-edit-btns">
+                        <button className="Driver-update-btn">Save changes</button>
+                        <button className="Driver-update-btn">Delete trip</button>
+                    </div>
                 </div>
-                <div className="Driver-update-passengers-items">
-                    {trip.passengers.map( passenger => (
-                        <>
-                            <Passenger key={passenger.passenger._id} passenger={passenger}/>
-                            <button>Remove passenger</button>
-                        </>
-                    ))}
-                </div>
-            </div>
-            <div className="Driver-update-edit-btns">
-                <button className="Driver-update-save">Save changes</button>
-                <button className="Driver-update-delete">Delete trip</button>
-            </div>
-        </div>
+                // <></>
+            ) : (
+                <></>
+            )}
+        </>
     )
 }
