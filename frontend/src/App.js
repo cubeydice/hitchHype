231dc64788fa52a17ee603862641f8cc598df7a0
Andoniank <andoniank@gmail.com>
2023-10-26 17:16:18 -0700

    change Authroute to route for '/'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
       <div className='main-content'>
       <NavBar />
       <Switch>
-        <Route exact path="/" component={MainPage} />
+        <Route exact path="/"component={MainPage} />
         <ProtectedRoute exact path="/account" component={AccountPage} />
         <ProtectedRoute exact path="/car" component={CarPage} />
         <Route exact path="/trips" component={ Trips } />
