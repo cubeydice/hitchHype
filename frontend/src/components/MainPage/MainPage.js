900f720ebf93797eb7cdbb675245fe88ba838f82
Raymond <raymondtan676@gmail.com>
2023-10-26 19:49:45 -0700

    Add icon and images to main page

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,9 +1,15 @@
+import { NavLink } from 'react-router-dom'
 import './MainPage.css'
 import roadtrip from '../../assets/images/dino-reichmuth-A5rCN8626Ck-unsplash.jpg'
-import { NavLink } from 'react-router-dom'
+import ecoImg from '../../assets/images/avenue-2215317_640.jpg'
+import gasImg from '../../assets/images/vendo-2589131_640.jpg'
+import carSharing from '../../assets/icons/car-sharing.png'
+import piggyBank from '../../assets/icons/piggy-bank.png'
+import planetEarth from '../../assets/icons/planet-earth.png'
 
 function MainPage() {
     return (
+        <div className='splash-container'>
         <div className='home-page'>
             <div className='header-container'>
                 <h1 className='home-header'>Commute. Connect. Save.</h1>
@@ -31,34 +37,40 @@ function MainPage() {
             </div>
             <div className='positive-container'>
                 <div className='positive-note'>
+                    <img className='piggy-icon' src={piggyBank} alt=''/>
                     <h2>Save on gas</h2>
                     <p>A cost-effective way to reduce your expenses on fuel and transportation.</p>
                 </div>
                 <div className='positive-note'>
+                    <img className='car-icon' src={carSharing} alt=''/>
                     <h2>Skip on driving</h2>
                     <p>Allows you to skip the stress of driving alone by sharing the ride with others.</p>
                 </div>
                 <div className='positive-note'>
+                    <img className='planet-icon' src={planetEarth} alt=''/>
                     <h2>Save the planet</h2>
                     <p>An environmentally-friendly choice that helps reduce the carbon footprint and lower emissions.</p>
                 </div>
             </div>
             <div className='positive-source-container'>
                 <div className='positive-source'>
-                    <img className='source-image' src="https://via.placeholder.com/350x300"/>
-                    <NavLink className='source-redirect'
-                        to="https://www.google.com/" 
-                        target="_blank"
-                    >X</NavLink> 
+                    <a className='source-redirect'
+                            href="https://www.google.com/" 
+                            target="_blank">
+                    <img className='source-image' src={gasImg}/>
+                    </a> 
+                    <span className='source-text'>Rising gas prices & inflation</span>
                 </div>
                 <div className='positive-source'>
-                    <img className='source-image' src="https://via.placeholder.com/350x300"/>
-                    <NavLink className='source-redirect'
-                        to="https://www.google.com/" 
-                        target="_blank"
-                    >X</NavLink> 
+                    <a className='source-redirect'
+                        href="https://www.google.com/" 
+                        target="_blank">
+                    <img className='source-image' src={ecoImg}/>
+                    </a> 
+                    <span className='source-text'>Sustainable travel, economic benefits.</span>
                 </div>
             </div>
+        </div>
         </div>
     );
 }
