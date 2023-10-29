7a96b6223b8e5cdf2231850ed775f78548bbeb61
Raymond <raymondtan676@gmail.com>
2023-10-28 18:30:13 -0700

    Create reviews validation input

--- backend/validations/review.js
+++ backend/validations/review.js
@@ -8,6 +8,18 @@ const validateReviewInput = [
     check('reviewee')
         .exists({ checkFalsy: true })
         .withMessage('Reviewer is required'),
+    check('rating')
+        .exists({ checkFalsy: true })
+        .isInt({ min: 1, max: 5 })
+        .withMessage('Rating must be between 1 and 5'),
+    check('title')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 5, max: 100 })
+        .withMessage('Title must be between 5 and 100 characters'),
+    check('body')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 50, max: 1000 })
+        .withMessage('Title must be between 50 and 1,000 characters'),
     handleValidationErrors
 ];
 
