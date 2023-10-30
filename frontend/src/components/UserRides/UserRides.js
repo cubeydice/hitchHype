a77b8e7cd277546485c97b29044bd60115a7efb2
Brittiny Filbert <britfilbert@icloud.com>
2023-10-29 20:23:13 -0700

    .

--- frontend/src/components/UserRides/UserRides.js
+++ frontend/src/components/UserRides/UserRides.js
@@ -7,8 +7,8 @@ import { useState } from "react";
 
 export function UserRides () {
     const dispatch = useDispatch();
-    const [trips, setTrips] = useState();
-    // const trips = Object.values(useSelector(state => state.trips))
+    // const [trips, setTrips] = useState();
+    const trips = Object.values(useSelector(state => state.trips))
     const user = useSelector(state => state.session.user)
     useEffect(() => {
 // <<<<<<< TripPagesStyling
@@ -20,28 +20,26 @@ export function UserRides () {
 // >>>>>>> main
 
     return(
-        <div className="user-rider-layout">
-
-        <div className="user-trips-container">
-            <div className="user-trips-nav-container">
-                < UserNavBar />
-            </div>
-            <div >
-                <div className="user-trips-header-container">
-                    <h3 className="user-trips-header-h3">Your Rides 🚌</h3>
+        <div className="user-trips-layout">
+            <div className="user-trips-container">
+                <div className="user-trips-nav-container">
+                    < UserNavBar />
                 </div>
-                <div className="user-trips-index-container">
-                    { trips && !trips.empty ? (
-                        trips.map(trip => (
-                            // <TripsItem key={trip._id} trip={trip} />
-                           (trip.driver._id !== user._id) ? <TripsItem key={trip._id} trip={trip} /> : ""
-                       ))
-                    ) : (
-                        <></>
-                    )}
+                <div >
+                    <div className="user-trips-header-container">
+                        <h3 className="user-trips-header-h3">Your Rides 🚌</h3>
+                    </div>
+                    <div className="user-trips-index-container">
+                        { trips && !trips.empty ? (
+                            trips.map(trip => (
+                                // <TripsItem key={trip._id} trip={trip} />
+                            (trip.driver._id !== user._id) ? <TripsItem key={trip._id} trip={trip} /> : ""
+                        ))
+                        ) : (
+                            <></>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    )
-}
+    )}
