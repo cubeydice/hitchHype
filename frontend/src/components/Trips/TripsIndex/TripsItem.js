f1157ddee8c5b68e037054e167e2092190ba82f3
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-10-30 02:24:07 -0700

    Add fetch for photo ref

--- frontend/src/components/Trips/TripsIndex/TripsItem.js
+++ frontend/src/components/Trips/TripsIndex/TripsItem.js
@@ -2,18 +2,39 @@ import sfPic from "../../../assets/icons/sf-img.jpg"
 import "./TripsItem.css"
 import explodeAddress from "../AddressParser"
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useEffect, useState } from "react";
+
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 
 export function TripsItem ({ trip }) {
     const id = useParams();
-    let city
+    const showPage = '/trips/' + trip._id;
+    const price = '$0'
+    // const [image, setImage] = useState(sfPic);
+
+    //GET ADDRESS
+    let city;
 
     explodeAddress(trip.destination, function(err,addressStr)
     {
         city = addressStr.city;
     })
-    const showPage = '/trips/' + trip._id;
 
-    const price = '$0'
+    //GET PLACE IMAGE
+    // const fetchImage = async () => {
+    //     try{
+    //     const response = await fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${city}/&key=${apiKey}`)
+    //     const data = await response.json();
+
+    //     console.log(data)
+    //   } catch (error) {
+    //     console.log("failed")
+    //   }
+    // }
+
+    // useEffect(()=>{
+    //     fetchImage()
+    // }, [])
 
     return (
         <a href={ showPage } className="TripItem-container">
