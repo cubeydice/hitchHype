c637f2fde58d0852626dde031298f763d8ad2d91
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-10-30 17:32:40 -0700

    Add review components

---
+++ frontend/src/components/Profile/Reviews/ReviewItem.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const ReviewItem = ( {userId} ) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+    }, [dispatch])
+
+    return (
+        <>
+        </>
+    )
+};
+
+export default ReviewItem;
