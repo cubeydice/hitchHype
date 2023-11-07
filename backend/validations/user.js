acb0ff9e21d83eebe2772e09dbb540b721c8be78
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-11-07 14:52:39 -0800

    Update biography error handling typo on backend

--- backend/validations/user.js
+++ backend/validations/user.js
@@ -17,7 +17,7 @@ const validateUserInput = [
     check('biography')
         .exists({ checkFalsy: true })
         .isLength({ max: 500 })
-        .withMessage('Password must not exceed 500 characters'),
+        .withMessage('Biography must not exceed 500 characters'),
     handleValidationErrors
 ];
 
